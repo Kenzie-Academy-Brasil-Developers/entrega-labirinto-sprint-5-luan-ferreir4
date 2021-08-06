14a9e8f7b061917b81a3253ee0c562c0996db944
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-08-06 14:57:20 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const mainContainer = document.getElementById("mainContent");
+
+const map = [
+    "WWWWWWWWWWWWWWWWWWWWW",
+    "W   W     W     W W W",
+    "W W W WWW WWWWW W W W",
+    "W W W   W     W W   W",
+    "W WWWWWWW W WWW W W W",
+    "W         W     W W W",
+    "W WWW WWWWW WWWWW W W",
+    "W W   W   W W     W W",
+    "W WWWWW W W W WWW W F",
+    "S     W W W W W W WWW",
+    "WWWWW W W W W W W W W",
+    "W     W W W   W W W W",
+    "W WWWWWWW WWWWW W W W",
+    "W       W       W   W",
+    "WWWWWWWWWWWWWWWWWWWWW",
+];
+
+for(let i= 0; i<map.length;i++){
+
+}
+mainContainer.innerHTML = map;
