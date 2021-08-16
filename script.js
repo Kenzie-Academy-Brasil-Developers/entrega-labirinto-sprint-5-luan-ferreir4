15c672e2cd21e9bd4d149c403b766d95057f3c5d
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-08-16 19:37:29 -0300

    Jogo funcional

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 const mainContainer = document.getElementById("mainContent");
+const resetBtn = document.getElementById('resetBtn');
+
 const player = document.createElement("div")
 player.classList.add("playerStyle");
 player.id = 'player'
-let currentPosition = [];
-const map = [
-    "WWWWWWWWWWWWWWWWWWWWW",
-    "W   W     W     W W W",
-    "W W W WWW WWWWW W W W",
-    "W W W   W     W W   W",
-    "W WWWWWWW W WWW W W W",
-    "W         W     W W W",
-    "W WWW WWWWW WWWWW W W",
-    "W W   W   W W     W W",
-    "W WWWWW W W W WWW W F",
-    "S     W W W W W W WWW",
-    "WWWWW W W W W W W W W",
-    "W     W W W   W W W W",
-    "W WWWWWWW WWWWW W W W",
-    "W       W       W   W",
-    "WWWWWWWWWWWWWWWWWWWWW",
-];
+
+
 
 function createMaze(){
+    const map = [
+        "WWWWWWWWWWWWWWWWWWWWW",
+        "W   W     W     W W W",
+        "W W W WWW WWWWW W W W",
+        "W W W   W     W W   W",
+        "W WWWWWWW W WWW W W W",
+        "W         W     W W W",
+        "W WWW WWWWW WWWWW W W",
+        "W W   W   W W     W W",
+        "W WWWWW W W W WWW W F",
+        "S     W W W W W W WWW",
+        "WWWWW W W W W W W W W",
+        "W     W W W   W W W W",
+        "W WWWWWWW WWWWW W W W",
+        "W       W       W   W",
+        "WWWWWWWWWWWWWWWWWWWWW",
+    ];
+
     for(let i= 0; i<map.length;i++){
         let rowDiv = document.createElement("div");
         rowDiv.classList.add("rowStyle")
@@ -37,19 +41,21 @@ function createMaze(){
             cellDiv.dataset.line = i;
             cellDiv.dataset.col  = j;
 
+
             if(cell === " "){
             cellDiv.classList.add("pathStyle") 
             }
             else if(cell === 'W'){
-                cell = '';
                 cellDiv.classList.add("wallStyle");
             }
             else if(cell === 'S'){ 
-                cellDiv.id = "start"
+                cellDiv.id = 'start';
+                cellDiv.classList.add("pathStyle")
                 cellDiv.appendChild(player);
             }
             else if(cell === 'F'){
                 cellDiv.id = "end"
+                cellDiv.classList.add("pathStyle")
             }
             cellDiv.append(cell);
 
@@ -58,28 +64,59 @@ function createMaze(){
     mainContainer.appendChild(rowDiv);
     }
 }
-createMaze();
+
 
 function moviments(event){
     const pressedKey = event.key;
-    let currentLine = player.parentElement.getAttribute('data-line');
-    let currentCol = player.parentElement.getAttribute('data-col')
+    let currentLine = Number(player.parentElement.getAttribute('data-line'));
+    let currentCol = Number(player.parentElement.getAttribute('data-col'));
+    let nextDiv;
 
         if(pressedKey === 'ArrowUp'){
+            nextDiv = mainContainer.children[currentLine - 1].children[currentCol];
 
+            if(nextDiv.classList.contains('pathStyle')){
+                nextDiv.appendChild(player)
+            }
         }
         else if(pressedKey === 'ArrowDown'){
-                
+            nextDiv = mainContainer.children[currentLine + 1].children[currentCol];
+            
+            if(nextDiv.classList.contains('pathStyle')){
+                nextDiv.appendChild(player)
+            }
         }
 
         if(pressedKey === 'ArrowLeft'){
-            console.log()
+            nextDiv = mainContainer.children[currentLine].children[currentCol - 1]
+
+            if(nextDiv.classList.contains('pathStyle')){
+                nextDiv.appendChild(player)
+            }
         }
         else if(pressedKey === 'ArrowRight'){
-            let nextDiv =  document.querySelector([Number(currentCol)+1])
-            // nextDiv.appendChild(player)
-            console.log(nextDiv);
+            nextDiv = mainContainer.children[currentLine].children[currentCol+1];
+
+            if(nextDiv.classList.contains('pathStyle')){
+                nextDiv.appendChild(player)
+            }
         }
-              
+        
+        checkOut(nextDiv);    
 }
-document.addEventListener('keydown',moviments)
+
+const checkOut = (div) =>{
+    const sucess = document.getElementById('sucessScreen');
+
+    if(div.id === 'end'){
+        sucess.classList.remove('hidden');
+    }
+}
+
+const resetMaze = () => {
+    const start = document.getElementById('start')
+    start.appendChild(player);
+}
+createMaze();
+document.addEventListener('keydown',moviments);
+resetBtn.addEventListener('click', resetMaze);
